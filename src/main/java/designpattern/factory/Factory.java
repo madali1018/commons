18f80494b69d4a05af88423bbf8b3494244e2590
madayiban <madayiban@163.com>
2017-11-08 21:43:56 +0800

    1108_3 1：update

---
+++ src/main/java/designpattern/factory/Factory.java
@@ -0,0 +1,14 @@
+package designpattern.factory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by madl on 2017/11/8.
+ */
+public class Factory {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Factory.class);
+
+
+}
---
+++ src/main/java/designpattern/factory/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by madl on 2017/11/8.
+ */
+package designpattern.factory;
+
+//简单工厂模式    Simple Factory Pattern
+//工厂方法模式    Factory Method Pattern
+//抽象工厂模式    Abstract Factory Pattern
