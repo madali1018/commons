33cbfb7fac229d565eed3f28783b004b257fee41
madayiban <madayiban@163.com>
2018-04-06 20:48:30 +0800

    Java static关键字demo

---
+++ src/test/java/staticdemo/pack3/Test.java
@@ -0,0 +1,42 @@
+package staticdemo.pack3;
+
+/**
+ * Created by madali on 2018/4/6.
+ */
+public class Test {
+    Person person = new Person("Test");
+
+    static {
+        System.out.println("test static");
+    }
+
+    public Test() {
+        System.out.println("test constructor");
+    }
+
+    public static void main(String[] args) {
+        new MyClass();
+    }
+}
+
+class Person {
+    static {
+        System.out.println("person static");
+    }
+
+    public Person(String str) {
+        System.out.println("person " + str);
+    }
+}
+
+class MyClass extends Test {
+    Person person = new Person("MyClass");
+
+    static {
+        System.out.println("myclass static");
+    }
+
+    public MyClass() {
+        System.out.println("myclass constructor");
+    }
+}
