7ccfdfde6fc0b9cbc7222c6bbf8169814627b76b
madayiban <madayiban@163.com>
2018-04-21 21:52:57 +0800

    移出设计模式包

--- src/main/java/designpattern/factory/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by madali on 2017/11/8.
- */
-package designpattern.factory;
-
-//简单工厂模式    Simple Factory Pattern
-//工厂方法模式    Factory Method Pattern
-//抽象工厂模式    Abstract Factory Pattern
--- src/main/java/designpattern/proxy/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by madali on 2017/11/8.
- */
-package designpattern.proxy;
-
-//代理模式
