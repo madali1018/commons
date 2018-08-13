e7fa056be6076a7167b09a021b506fc3d02cc280
madayiban <madayiban@163.com>
2018-08-13 16:13:16 +0800

    update fastjson demo

---
+++ src/main/java/enumeration/A.java
@@ -0,0 +1,14 @@
+package enumeration;
+
+/**
+ * @Auther: madali
+ * @Date: 2018/8/8 16:08
+ */
+public class A {
+
+    //可变长度的参数只能有一个，且必须是方法的最后一个参数
+    public void a(String s1, String... s2) {
+
+    }
+
+}
