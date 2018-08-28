2b4975332e0b33eeb40d33bf68d26e4403d87c64
madali <0522Mada0522>
2018-08-28 19:43:46 +0800

    负载均衡算法更新

--- src/main/java/com/mada/common/util/balance/RoundRobin.java
+++ src/main/java/com/mada/common/util/balance/RoundRobin.java
@@ -1,5 +1,8 @@
 package com.mada.common.util.balance;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,55 +12,58 @@ import java.util.List;
  */
 public class RoundRobin {
 
-    private List<String> serverList = new ArrayList<>();  //服务集合
-    private int index = -1;                   //上一次调度下标
+    private static final Logger LOGGER = LoggerFactory.getLogger(RoundRobin.class);
+
+    //上一次调度下标
+    private int index = -1;
+
+    //服务名称集合
+    private List<String> serverList = new ArrayList<>();
 
     /**
      * 添加节点
      *
-     * @param server
+     * @param serverName
      */
-    public synchronized void addNode(String server) {
-
-        serverList.add(server);
+    public synchronized void addNode(String serverName) {
+        serverList.add(serverName);
+        LOGGER.info("添加服务节点:{}成功", serverName);
     }
 
     /**
      * 删除节点
      *
-     * @param server
+     * @param serverName
      */
-    public synchronized void removeNode(String server) {
-
+    public synchronized void removeNode(String serverName) {
         for (int i = 0; i < serverList.size(); i++) {
-            if (serverList.get(i).equals(server)) {
+            if (serverList.get(i).equals(serverName)) {
                 serverList.remove(i);
-
+                LOGGER.info("删除服务节点:{}成功", serverName);
                 break;
             }
         }
     }
 
     /**
-     * 获取下一个请求的server服务
+     * 获取下一个请求的server服务的名称
      *
      * @return
      */
     public synchronized String next() {
+        String serverName = null;
 
-        String server = null;
-
-        if (serverList.size() == 0)
+        if (serverList.size() == 0) {
             index = -1;
-        else {
+        } else {
             index++;
-
-            if (index >= serverList.size())
+            if (index >= serverList.size()) {
                 index = 0;
-
-            server = serverList.get(index);
+            }
+            serverName = serverList.get(index);
+            LOGGER.info("获取到的服务节点:{}", serverName);
         }
 
-        return server;
+        return serverName;
     }
 }
---
+++ src/main/java/com/mada/common/util/balance/RoundRobinDemo.java
@@ -0,0 +1,25 @@
+package com.mada.common.util.balance;
+
+/**
+ * @Auther: madali
+ * @Date: 2018/8/20 14:04
+ */
+public class RoundRobinDemo {
+
+    public static void main(String[] args) {
+
+        RoundRobin robin = new RoundRobin();
+        for (int i = 0; i < 10; i++) {
+            robin.addNode("10" + i);
+        }
+
+        robin.removeNode("102");
+        System.out.println("------------");
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("serverName:" + robin.next());
+        }
+
+    }
+
+}
