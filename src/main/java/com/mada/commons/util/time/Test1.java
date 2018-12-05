7cc42b504cab7091504fac8ea36cfff195b5c963
madali <0522Mada0522>
2018-12-05 10:26:03 +0800

    java8 time测试类更新

--- src/main/java/com/mada/commons/util/time/Test1.java
+++ src/main/java/com/mada/commons/util/time/Test1.java
@@ -3,17 +3,22 @@ package com.mada.commons.util.time;
 import org.junit.Test;
 
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.time.temporal.ChronoField;
 
 /**
  * @Auther: madali
  * @Date: 2018/8/7 10:36
  */
-public class Demo {
+public class Test1 {
 
     @Test
     public void test1() {
+        System.out.println("当前GMT0时间:" + Instant.now());
+        System.out.println("当前机器所在时区的时间:" + OffsetDateTime.now());
+
         //获取当前时间的年
         int year = LocalDate.now().get(ChronoField.YEAR);
         System.out.println(year);
