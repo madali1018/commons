e057801d6c49d3fc7986efe7591b565313b6c027
madayiban@163.com <madayiban@163.com>
2019-04-10 17:42:26 +0800

    线程池等待所有线程执行完毕.使用比AtomicLong还高效的LongAdder进行数据统计

--- src/main/java/com/mada/commons/demo/threadpool/p3/Test1.java
+++ src/main/java/com/mada/commons/demo/threadpool/p3/Test1.java
@@ -1,89 +1,43 @@
 package com.mada.commons.demo.threadpool.p3;
 
-import java.time.OffsetDateTime;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
 
 /**
- * Created by madali on 2019/4/8 15:22
+ * Created by madali on 2019/4/10 17:22
  */
+@Slf4j
 public class Test1 {
 
-    private static int i = 0;
+    private static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10, 20, 30, TimeUnit.MILLISECONDS,
+            new ArrayBlockingQueue<>(500), new ThreadPoolExecutor.CallerRunsPolicy());
+
+    private static CountEntity countEntity = CountEntity.builder().num1(new LongAdder()).num2(new LongAdder()).build();
 
     public static void main(String[] args) {
-        t1();
-    }
 
-    private static void t1() {
-        ScheduledExecutorService exc = Executors.newSingleThreadScheduledExecutor();
-
-        // scheduleAtFixedRate 是以上一个任务开始的时间计时，10秒过去后，检测上一个任务是否执行完毕，如果上一个任务执行完毕，则当前任务立即执行，如果上一个任务没有执行完毕，则需要等上一个任务执行完毕后立即执行。
-        exc.scheduleAtFixedRate(() -> {
-            try {
-                i++;
-                if (i == 4) {
-                    throw new RuntimeException();
-                } else {
-                    System.out.println(OffsetDateTime.now() + "," + i);
-                }
-
-                if (i == 2) {
-                    try {
-                        Thread.sleep(10 * 1000L);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-            } catch (RuntimeException ex) {
-                System.out.println("在ScheduledExecutorService中有异常抛出，异常堆栈：" + ex.getStackTrace());
-            }
-        }, 0, 10, TimeUnit.SECONDS);
-    }
-
-    private static void t2() {
-        ScheduledExecutorService exc = Executors.newSingleThreadScheduledExecutor();
-        // scheduleWithFixedDelay 是以上一个任务结束时开始计时，10秒过去后，立即执行。
-        exc.scheduleWithFixedDelay(() -> {
-            try {
-                i++;
-                if (i == 6) {
-                    throw new RuntimeException();
-                } else {
-                    System.out.println(OffsetDateTime.now() + "," + i);
-                }
-            } catch (Exception ex) {
-                System.out.println();
-                System.out.println("在ScheduledExecutorService中有异常抛出，异常堆栈：" + ex.getStackTrace());
-            }
-        }, 0, 10, TimeUnit.SECONDS);
-    }
-
-    private static void t3() {
-        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-//        ScheduledExecutorService service2 = Executors.newScheduledThreadPool(10);
-//        service.scheduleAtFixedRate(() -> System.out.println("now:"+ OffsetDateTime.now()), 0, 2, TimeUnit.SECONDS);
-
-        ScheduledFuture future = service.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                i++;
-                if (i == 6) {
-                    throw new RuntimeException();
-                } else {
-                    System.out.print(i + " ");
-                }
-            }
-        }, 0, 1, TimeUnit.SECONDS);
-
-        try {
-            future.get();
-        } catch (Exception e) {
-            System.out.println("在ScheduledExecutorService中有异常抛出，异常堆栈：" + e.getStackTrace());
+        for (int i = 0; i < 100000; i++) {
+            threadPoolExecutor.execute(new MyTask(i, countEntity));
         }
+
+        threadPoolExecutor.shutdown();
+
+        // 线程池中线程都执行完毕后再 记录 统计数据
+        while (true) {
+            try {
+                // 每10ms执行一次判断操作：线程池中线程是否已完全执行完
+                if (threadPoolExecutor.awaitTermination(10, TimeUnit.MILLISECONDS)) break;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        log.info("统计结果{}", countEntity);
+
     }
 
 }
