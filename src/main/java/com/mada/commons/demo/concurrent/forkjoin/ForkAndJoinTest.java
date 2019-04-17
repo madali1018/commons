0ee38503be313c727ae962a57684ef9170e47e53
madayiban@163.com <madayiban@163.com>
2019-04-17 19:39:05 +0800

    ForkJoinPool示例:RecursiveTask和RecursiveAction

--- src/main/java/com/mada/commons/demo/concurrent/forkjoin/ForkAndJoinTest.java
+++ src/main/java/com/mada/commons/demo/concurrent/forkjoin/ForkAndJoinTest.java
@@ -3,11 +3,14 @@ package com.mada.commons.demo.concurrent.forkjoin;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.TimeUnit;
 
 /**
- * fork/join框架demo
+ * fork/join
  * <p>
- * RecursiveTask：用于有返回值的任务。
+ * RecursiveTask：用于有返回值的任务
+ * <p>
+ * 1.Java8 Stream 的实现原理本质上就是在ForkJoin上进行了一层封装，将Stream 不断尝试分解成更小的split，然后使用fork/join 框架分而治之
  *
  * @Auther: madali
  * @Date: 2018/8/31 16:34
@@ -15,7 +18,7 @@ import java.util.concurrent.RecursiveTask;
 public class ForkAndJoinTest extends RecursiveTask<Integer> {
 
     // 设立一个最大计算容量
-    private static final int DEFAULT_CAPACITY = 100;
+    private static final int MAX = 100;
 
     // 用2个数字表示目前要计算的范围
     private int start;
@@ -31,12 +34,13 @@ public class ForkAndJoinTest extends RecursiveTask<Integer> {
         int sum = 0;
 
         // 如果任务量在最大容量之内：计算任务
-        boolean canCompute = (end - start) <= DEFAULT_CAPACITY;
+        boolean canCompute = (end - start) <= MAX;
         if (canCompute) {
             for (int i = start; i <= end; i++) {
                 sum += i;
             }
         } else {
+            System.out.println("thread:" + Thread.currentThread().getName() + ", range:[" + start + ", " + end + "]");
             // 计算容量中间值：如果超过了最大容量，那么就进行拆分处理
             int middle = (start + end) / 2;
             // 递归
@@ -59,6 +63,12 @@ public class ForkAndJoinTest extends RecursiveTask<Integer> {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         // 任务执行类(ForkJoinTask的子类)：生成一个计算任务，计算1+2+3+...+10000
         ForkAndJoinTest task = new ForkAndJoinTest(1, 10000);
+
+        //阻塞当前线程直到 ForkJoinPool 中所有的任务都执行结束
+        forkJoinPool.awaitTermination(2, TimeUnit.SECONDS);
+
+        // 关闭线程池
+        forkJoinPool.shutdown();
 
         long result = forkJoinPool.invoke(task);
         System.out.println(result);
---
+++ src/main/java/com/mada/commons/demo/concurrent/forkjoin/ForkAndJoinTest2.java
@@ -0,0 +1,56 @@
+package com.mada.commons.demo.concurrent.forkjoin;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * RecursiveAction：用于没有返回值的任务。
+ * <p>
+ * Created by madali on 2019/4/17 16:31
+ */
+public class ForkAndJoinTest2 extends RecursiveAction {
+
+    private static final int MAX = 20;
+
+    private int start;
+    private int end;
+
+    public ForkAndJoinTest2(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected void compute() {
+        //当end-start的值小于MAX时，开始打印
+        if ((end - start) < MAX) {
+            for (int i = start; i < end; i++) {
+                System.out.println(Thread.currentThread().getName() + "i的值" + i);
+            }
+        } else {
+            System.out.println("thread:" + Thread.currentThread().getName() + ", range:[" + start + ", " + end + "]");
+            // 将大任务分解成两个小任务
+            int middle = (start + end) / 2;
+            ForkAndJoinTest2 left = new ForkAndJoinTest2(start, middle);
+            ForkAndJoinTest2 right = new ForkAndJoinTest2(middle, end);
+            left.fork();
+            right.fork();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // 创建包含Runtime.getRuntime().availableProcessors()返回值作为个数的并行线程的ForkJoinPool
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+
+        // 提交可分解的PrintTask任务
+        forkJoinPool.submit(new ForkAndJoinTest2(0, 1000));
+
+        //阻塞当前线程直到 ForkJoinPool 中所有的任务都执行结束
+        forkJoinPool.awaitTermination(2, TimeUnit.SECONDS);
+
+        // 关闭线程池
+        forkJoinPool.shutdown();
+    }
+
+}
