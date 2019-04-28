014d55b60d880c6270a82e4f66aa4cbb7bdab2fb
madayiban@163.com <madayiban@163.com>
2019-04-28 16:44:39 +0800

    移除冗余代码

--- src/main/java/com/mada/commons/demo/cglib/demo2/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by madali on 2017/6/4.
- */
-package com.mada.commons.demo.cglib.demo2;
-
-//使用Cglib定义不同的拦截策略。再扩展一点点，比方说在AOP中我们经常碰到的一种复杂场景是：
-// 我们想对类A的B方法使用一种拦截策略、类A的C方法使用另外一种拦截策略。
-
--- src/main/java/com/mada/commons/demo/jvm/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * 本包下测试类来源：http://www.cnblogs.com/wade-xu/p/4369094.html
- *
- * @Auther: madali
- * @Date: 2018/8/20 18:12
- */
-package com.mada.commons.demo.jvm;
