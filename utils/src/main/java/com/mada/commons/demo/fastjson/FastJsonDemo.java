14cd12cc2d3a5699014cd13a281ce83c4ac11ec8
madayiban@163.com <madayiban@163.com>
2019-06-21 11:06:51 +0800

    fastjson使用示例更新

--- utils/src/main/java/com/mada/commons/demo/fastjson/FastJsonDemo.java
+++ utils/src/main/java/com/mada/commons/demo/fastjson/FastJsonDemo.java
@@ -24,111 +24,67 @@ public class FastJsonDemo {
         jsonObject.remove("a");
         jsonObject.remove("a2222");
         System.out.println(jsonObject);
-        System.out.println(jsonObject.toString());
+        System.out.println(jsonObject.containsKey("b"));
     }
 
-    // JSONObject JSONArray的基础使用
     @Test
-    public void test1() {
+    public void t2() {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("a", "1");
         jsonObject.put("b", "2");
         System.out.println(jsonObject);
-        System.out.println("----------------");
 
-        JSONArray jsonArray;
         String array = "[{\"groupid\": \"1.1.1.1\", \"sensorid\": 3, \"pressure\": \"1.125\",\"time\": 1508727704},{\"groupid\": \"1.1.1.1\", \"sensorid\": 3, \"pressure\": \"0.842\",\"time\": 1508727704}]";
-        jsonArray = JSONArray.parseArray(array);
+        JSONArray jsonArray = JSONArray.parseArray(array);
         jsonObject.put("array", jsonArray);
         System.out.println(jsonArray);
-        System.out.println("----------------");
-
-        String fieldsStr = "{\"groupid\":\"4.1.1.2.1\",\"sensorid\":2,\"time\":15215484298,\"type\":\"NH3\",\"value\":\"28.50\"}";
-        JSONObject fields = JSONObject.parseObject(fieldsStr);
-        System.out.println(fields);
-        System.out.println(fields.containsKey("time"));
+        System.out.println(jsonObject);
     }
 
-    // list map转 json
+    // list、map 和 json 互转
     @Test
-    public void test2() {
+    public void t3() {
         List<Person> list = new ArrayList<>();
-        Person p1 = new Person(1, "11");
-        Person p2 = new Person(2, "12");
+        Person p1 = Person.builder().id(1).address("11111").build();
+        Person p2 = Person.builder().id(2).address("22222").build();
         list.add(p1);
         list.add(p2);
 
         String json = JSON.toJSONString(list);
-        System.out.println(json);
-        System.out.println("--------------------------");
+        System.out.println("list转json:" + json);
+        List<Person> list2 = JSON.parseObject(json, new TypeReference<List<Person>>() {
+        });
+        System.out.println("json转list,遍历输出每个元素:");
+        list2.forEach(System.out::println);
+
+        System.out.println("------------");
 
         Map<Integer, Person> map = new HashMap<>();
         map.put(1, p1);
         map.put(2, p2);
         String json2 = JSON.toJSONString(map);
-        System.out.println(json2);
+        System.out.println("map转json:" + json2);
+        Map<Integer, Person> map2 = JSON.parseObject(json2, new TypeReference<Map<Integer, Person>>() {
+        });
+        System.out.println("json转map,遍历输出每个元素:");
+        map2.entrySet().stream().map(entry -> "key:" + entry.getKey() + ",value:" + entry.getValue()).forEach(System.out::println);
     }
 
-    @Test
-    public void t3_2() {
-        List<Long> list = new ArrayList<>();
-        list.add(12L);
-        list.add(13L);
-
-        String str = JSON.toJSONString(list);
-
-        List<Long> list2 = JSON.parseObject(str, new TypeReference<List<Long>>() {
-        });
-
-        list2.forEach(System.out::println);
-    }
-
-    // json 转 list map
-    @Test
-    public void test3() {
-        String json = "[{\"age\":1,\"name\":\"11\"},{\"age\":2,\"name\":\"12\"}]";
-        List<Person> list = JSON.parseObject(json, new TypeReference<List<Person>>() {
-        });
-        for (Person person : list) {
-            System.out.println(person);
-        }
-        System.out.println("----------------------");
-
-        String json2 = "{1:{\"age\":1,\"name\":\"11\"},2:{\"age\":2,\"name\":\"12\"}}";
-        Map<Integer, Person> map = JSON.parseObject(json2, new TypeReference<Map<Integer, Person>>() {
-        });
-        for (Map.Entry<Integer, Person> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + "," + entry.getValue());
-        }
-    }
-
-    // Java对象转json
+    // Java对象 和 json 互转
     @Test
     public void test4() {
-        Person p = new Person(1, "11");
+        Person p = Person.builder().id(1).age(18).name("中").address("11111").build();
         String json = JSON.toJSONString(p);
-        System.out.println(json);
-        System.out.println("-------------------------");
+        System.out.println("java对象转json:" + json);
+
+        String json2 = JSON.toJSONString(p, PropertyFilterImpl.INSTANCE);
+        System.out.println("java对象转json(使用自定义fastjson的拦截器过滤不需要的字段):" + json2);
 
         //json转Java对象
         Person p2 = JSONObject.parseObject(json, Person.class);
-        System.out.println(p2);
-        System.out.println("-------------------------");
-
-        //json转Java对象
         Person p3 = JSON.parseObject(json, Person.class);
-        System.out.println(p3);
-    }
-
-    // 对象转json：过滤不需要转换的字段
-    @Test
-    public void test5() {
-        Person p1 = new Person(1, 16, "11", "北京");
-        String json = JSON.toJSONString(p1);
-        System.out.println(json);
-        // 过滤不需要的字段：自定义fastjson的拦截器
-        String json2 = JSON.toJSONString(p1, PropertyFilterImpl.INSTANCE);
-        System.out.println(json2);
+        System.out.println("json转java对象:" + p2);
+        System.out.println("json转java对象:" + p3);
     }
 
     private static class PropertyFilterImpl implements PropertyFilter {
@@ -137,7 +93,7 @@ public class FastJsonDemo {
         @Override
         public boolean apply(Object object, String name, Object value) {
             //表示id和address字段将被排除在外
-            return !Objects.equals("address", name) && !Objects.equals("id", name);
+            return !(Objects.equals("address", name) || Objects.equals("id", name));
         }
     }
 
