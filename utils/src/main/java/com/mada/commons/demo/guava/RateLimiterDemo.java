49326bb43a0436cbeaf0e189f8993fd6659adab0
madayiban@163.com <madayiban@163.com>
2019-07-31 10:57:57 +0800

    RateLimiter使用示例更新

--- utils/src/main/java/com/mada/commons/demo/guava/RateLimiterDemo.java
+++ utils/src/main/java/com/mada/commons/demo/guava/RateLimiterDemo.java
@@ -5,6 +5,9 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
 
 import java.time.OffsetDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by madali on 2019/4/1 11:57
@@ -12,9 +15,10 @@ import java.time.OffsetDateTime;
 @Log4j2
 public class RateLimiterDemo {
 
-    // 限制频率
-    // 创建一个限流器，参数代表每秒生成的令牌数
-    private static final RateLimiter limiter = RateLimiter.create(2);
+    // 限制频率：创建一个限流器，参数代表每秒生成的令牌数，即每秒只发出多少个令牌
+    private static double permitsPerSecond = 5D;
+
+    private static RateLimiter limiter = RateLimiter.create(permitsPerSecond);
 
     @Test
     public void t1() {
@@ -28,13 +32,42 @@ public class RateLimiterDemo {
 
     @Test
     public void t2() {
-        for (int i = 1; i < 20; i++) {
+        for (int i = 0; i < 20; i++) {
             if (limiter.tryAcquire()) {
                 System.out.println("成功,i:" + i);
             } else {
                 System.out.println("失败,i:" + i);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        try {
+            access();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void access() throws Exception {
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 100; i++) {
+            final int num = i;
+
+            fixedThreadPool.submit(new Runnable() {
+                @Override
+                public void run() {
+                    if (limiter.tryAcquire()) {
+                        System.out.println("成功,num:" + num);
+                    } else {
+                        System.out.println("失败,num:" + num);
+                    }
+                }
+            });
+        }
+
+        fixedThreadPool.shutdown();
+        fixedThreadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
     }
 
 }
